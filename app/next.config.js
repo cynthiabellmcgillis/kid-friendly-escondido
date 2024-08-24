9b1dcaf462853fc4501b47d214603a775a6806ab
Cynthia Bell McGillis <12412365+cynthiabellmcgillis@users.noreply.github.com>
2024-08-24 13:52:01 -0700

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'], // Add any other domains you might use for images
+    domains: ['localhost'],
   },
 }
 
