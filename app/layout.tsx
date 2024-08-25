28aa1ef285ae81a53a12a33ef20f8aed937de09e
Cynthia Bell McGillis <12412365+cynthiabellmcgillis@users.noreply.github.com>
2024-08-25 13:27:12 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,19 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 
 export const metadata: Metadata = {
-  title: 'Kid Friendly Escondido',
+  title: 'Kid-Friendly Escondido',
   description: 'Discover kid-friendly activities in Escondido',
   openGraph: {
-    title: 'Kid Friendly Escondido',
+    title: 'Kid-Friendly Escondido',
     description: 'Discover kid-friendly activities in Escondido',
     url: 'https://kid-friendly-escondido.vercel.app/',
-    siteName: 'Kid Friendly Escondido',
+    siteName: 'Kid-Friendly Escondido',
     images: [
       {
         url: 'https://opengraph.b-cdn.net/production/images/6b3db2ff-ebac-4b39-8330-407c16ed66e6.png?token=kHTv_1mexwvh2LtWcee13rw7Vt4EJvJYEI4sgpAN708&height=600&width=1200&expires=33260614490',
         width: 1200,
         height: 600,
-        alt: 'Kid Friendly Escondido',
+        alt: 'Kid-Friendly Escondido',
       },
     ],
     locale: 'en_US',
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Kid Friendly Escondido',
+    title: 'Kid-Friendly Escondido',
     description: 'Discover kid-friendly activities in Escondido',
     site: '@cynthiamcgillis',
     creator: '@cynthiamcgillis',
