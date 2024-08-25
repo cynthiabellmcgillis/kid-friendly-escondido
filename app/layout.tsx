b1261168683fb2107982f61f9689ff121c4fa4e7
Cynthia Bell McGillis <12412365+cynthiabellmcgillis@users.noreply.github.com>
2024-08-25 12:32:23 -0700

    Update layout.tsx
    Added hypen.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,19 @@ import { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Kid Friendly Escondido',
+  title: 'Kid-Friendly Escondido',
   description: 'Discover kid-friendly activities in Escondido',
   openGraph: {
-    title: 'Kid Friendly Escondido',
+    title: 'Kid-Friendly Escondido',
     description: 'Discover kid-friendly activities in Escondido',
     url: 'https://kid-friendly-escondido.vercel.app/',
-    siteName: 'Kid Friendly Escondido',
+    siteName: 'Kid-Friendly Escondido',
     images: [
       {
         url: 'https://opengraph.b-cdn.net/production/images/187200cf-867d-4ca7-ba01-fee494dd9983.png?token=uqqMZ54UVa72XPALPHISFZ6v7MLvncMd0vrIbNzdOmE&height=600&width=1200&expires=33260554069',
         width: 1200,
         height: 600,
-        alt: 'Kid Friendly Escondido',
+        alt: 'Kid-Friendly Escondido',
       },
     ],
     locale: 'en_US',
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Kid Friendly Escondido',
+    title: 'Kid-Friendly Escondido',
     description: 'Discover kid-friendly activities in Escondido',
     site: 'cynthiamcgillis', // Replace with your actual Twitter handle
     creator: 'cynthiamcgillis', // Replace with your actual Twitter handle
