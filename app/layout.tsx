cca6df6ed84dc2ddca51d8d1394a0e7e809a87f4
Cynthia Bell McGillis <12412365+cynthiabellmcgillis@users.noreply.github.com>
2024-08-25 20:41:59 -0700

    Update layout.tsx
    updated og

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     description: 'Discover kid-friendly activities in Escondido',
     site: '@cynthiamcgillis',
     creator: '@cynthiamcgillis',
-    images: ['https://opengraph.b-cdn.net/production/images/6b3db2ff-ebac-4b39-8330-407c16ed66e6.png?token=kHTv_1mexwvh2LtWcee13rw7Vt4EJvJYEI4sgpAN708&height=600&width=1200&expires=33260614490'],
+    images: ['https://opengraph.b-cdn.net/production/images/f6564f81-f3e4-4c44-8cc6-945bd9790008.png?token=GVN8U7PHkfCtn_AgYeox1jqYQjx28lUFd24mua30xjU&height=590&width=1200&expires=33260643549'],
   },
   metadataBase: new URL('https://kid-friendly-escondido.vercel.app'),
 }
